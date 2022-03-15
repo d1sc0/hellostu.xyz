e684381225aa50fb6f503aba49f3bedd6395fcac
Stuart Mackenzie <ilovediscostu@gmail.com>
2022-03-15 08:52:36 +0000

    update tag list styles

--- README.md
+++ README.md
@@ -6,10 +6,12 @@
 ## 🚀 To-do list
 
 - update config, migrate content, styling on posts, post-lists - done
-- update styling on tag pages to match
+- update styling on tag pages to match - done
 - Design and make new home page
-- Migrate the about me page.
+- update other styles
+- Migrate the about me page
 - Improve Bio component
+- look at react leaflet and play with maps again
 - deploy on firebase using github actions
 - update twitter and open graph in seo component to use postImage or fallback gracefully to an icon use || ?
 
--- src/templates/post-list-by-tag-template.js
+++ src/templates/post-list-by-tag-template.js
@@ -11,23 +11,26 @@ const PostsByTagList = ({ data, pageContext }) => {
   const isLast = currentPage === numTagPages
   const prevPage = currentPage - 1 === 1 ? '' : (currentPage - 1).toString()
   const nextPage = (currentPage + 1).toString()
-  const pageTitle = `Tag: ${tagName} (${currentPage} of ${numTagPages})`
+  const pageTitle = `Tagged: ${tagName}`
   return (
     <Layout>
       <Seo title={pageTitle} />
-      <h1 className="title is-size-2">{pageTitle}</h1>
+      <h1 className="title is-size-4">{pageTitle}</h1>
       {posts.map(post => {
-        const tags = post.frontmatter.tags
         const title = post.frontmatter.title || post.slug
+        const tags = post.frontmatter.tags
         return (
-          <div className="block" key={post.id}>
-            <h2 className="is-size-4">
+          <div
+            className="block rounded-corners post-summary p-5 my-4"
+            key={post.id}
+          >
+            <h2 className="is-size-3 has-text-weight-bold">
               <Link to={`/posts/${post.slug}`}>{title}</Link>
             </h2>
-            <small className="is-size-7 mr-3">
-              Posted: {post.frontmatter.date}
-            </small>
-            <small className="is-size-7">
+            <p className="is-size-6">
+              <span className="is-size-7">
+                Posted: {post.frontmatter.date}{' '}
+              </span>
               {tags.map(tag => {
                 return (
                   <span key={tag} className="tag is-light mr-2">
@@ -35,13 +38,11 @@ const PostsByTagList = ({ data, pageContext }) => {
                   </span>
                 )
               })}
-            </small>
-
-            <p className="py-2">{post.excerpt}</p>
+            </p>
+            <p className="is-size-6 py-2">{post.excerpt}</p>
           </div>
         )
       })}
-
       {!isFirst && (
         <nav
           className="pagination py-4 is-pulled-left"
