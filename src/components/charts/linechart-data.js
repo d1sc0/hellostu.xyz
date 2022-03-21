0479d5eca4c790ce21bec46f288402333d48f7b0
Stuart Mackenzie <ilovediscostu@gmail.com>
2022-03-21 23:08:30 +0000

    blog post updated

--- src/components/charts/linechart-data.js
+++ src/components/charts/linechart-data.js
@@ -1,12 +1,14 @@
-export const options = {
+import moment from 'moment'
+
+export const options1 = {
   layout: {
     padding: 20,
   },
   responsive: true,
   scales: {
-    x: {
-      title: 'events',
-      display: true,
+    y: {
+      min: 0,
+      max: 400,
     },
   },
   plugins: {
@@ -16,12 +18,12 @@ export const options = {
     },
     title: {
       display: true,
-      text: 'Weekly Finishers at The Great Field',
+      text: 'Weekly finishers at The Great Field parkrun',
     },
   },
 }
 
-export const data = {
+export const data1 = {
   labels: [
     '30 Oct 21',
     '06 Nov 21',
@@ -52,7 +54,74 @@ export const data = {
         363, 243, 222, 243, 231, 230,
       ],
       fill: false,
-      borderColor: 'rgb(75, 192, 192)',
+      borderColor: '#955196',
+      borderWidth: 5,
+      tension: 0.2,
+    },
+  ],
+}
+
+export const options2 = {
+  layout: {
+    padding: 20,
+  },
+  responsive: true,
+  scales: {
+    y: {
+      min: moment.duration(3, 'minutes'),
+      max: moment.duration(40, 'minutes'),
+      type: 'time',
+      time: {
+        unit: 'minute',
+        displayFormats: 'mm',
+      },
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Times of fastest finisher weekly',
+    },
+  },
+}
+
+export const data2 = {
+  labels: [
+    '30 Oct 21',
+    '06 Nov 21',
+    '13 Nov 21',
+    '20 Nov 21',
+    '27 Nov 21',
+    '04 Dec 21',
+    '11 Dec 21',
+    '18 Dec 21',
+    '01 Jan 22',
+    '08 Jan 22',
+    '15 Jan 22',
+    '22 Jan 22',
+    '29 Jan 22',
+    '05 Feb 22',
+    '12 Feb 22',
+    '19 Feb 22',
+    '26 Feb 22',
+    '05 Mar 22',
+    '12 Mar 22',
+    '19 Mar 22',
+  ],
+  datasets: [
+    {
+      label: 'Fastest time',
+      data: [
+        /* moment.duration(997, 'seconds'), */
+        moment.duration(997, 'seconds').asMinutes,
+      ],
+      fill: false,
+      borderColor: '#ff6e54',
+      borderWidth: 5,
       tension: 0.2,
     },
   ],
--- src/components/charts/linechart.js
+++ src/components/charts/linechart.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
-import { options, data } from './linechart-data.js'
+import { options1, data1, options2, data2 } from './linechart-data.js'
 import { Line } from 'react-chartjs-2'
+import 'chartjs-adapter-moment'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,8 +11,12 @@ import {
   Title,
   Tooltip,
   Legend,
+  TimeScale,
+  TimeSeriesScale,
 } from 'chart.js'
 ChartJS.register(
+  TimeScale,
+  TimeSeriesScale,
   CategoryScale,
   LinearScale,
   PointElement,
@@ -21,10 +26,21 @@ ChartJS.register(
   Legend
 )
 
-const Linechart = () => {
+const Linechart = ({ data }) => {
+  let optionsfile = ''
+  let datafile = ''
+
+  if (data === 'attendance') {
+    optionsfile = options1
+    datafile = data1
+  }
+  if (data === 'fasttimes') {
+    optionsfile = options2
+    datafile = data2
+  }
   return (
     //this is a comment
-    <Line options={options} data={data} />
+    <Line options={optionsfile} data={datafile} />
   )
 }
 
