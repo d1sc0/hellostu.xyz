5a968edac3aac9cc84aff1ad4f583370629349fa
Stuart Mackenzie <ilovediscostu@gmail.com>
2023-01-26 22:37:56 +0000

    remove dog owner

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
           FutureGov) changing and improving our public services. He's also known for being a father, husband, runner,
           photography nerd, dog owner and excotic disco dancer!`,
     },
-    description: `hello stu is the website of Stuart Mackenzie - Stuart is a Senior Partner at TPXimpact (previously known as FutureGov). I'm also known for being a father, husband, runner, photography nerd, dog owner and excotic disco dancer!`,
+    description: `hello stu is the website of Stuart Mackenzie - Stuart is a Senior Partner at TPXimpact (previously known as FutureGov). He is also known for being a father, husband, runner, photography nerd and excotic disco dancer!`,
     siteUrl: `https://hellostu.xyz`,
     social: {
       twitter: `_disco`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const HomePage = () => {
           I work at <a href="https://tpximpact.com">TPXimpact</a> (previously
           known as <a href="https://wearefuturegov.com">FutureGov</a>
           ). I'm also known for being a father, husband, runner, photography
-          nerd, dog owner and excotic disco dancer!
+          nerd and excotic disco dancer!
         </p>
         <p>
           I occasionally use this site to{' '}
