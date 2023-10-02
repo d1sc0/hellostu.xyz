15b036564d517e64f9c0de2107ec0668428e176f
Stuart Mackenzie <ilovediscostu@gmail.com>
2023-10-02 15:34:41 +0100

    update to player

--- src/components/podcast.js
+++ src/components/podcast.js
@@ -7,6 +7,18 @@ const Podcast = ({ audio }) => {
         <source src={audio} type="audio/mpeg" />
         Your browser does not support the audio tag.
       </audio>
+      <small>
+        Subscribe in your podcast player:{' '}
+        <a href="https://podcasts.apple.com/us/podcast/hello-stu/id1709876510">
+          Apple
+        </a>{' '}
+        |{' '}
+        <a href="https://open.spotify.com/show/21LDbQx6BlTudyfoT3oQH5">
+          Spotify
+        </a>{' '}
+        | <a href="#">Google (TBC)</a> |{' '}
+        <a href="https://hellostu.xyz/podcast.xml">Other (RSS)</a>
+      </small>
     </div>
   )
 }
