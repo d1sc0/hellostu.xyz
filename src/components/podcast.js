9f8f0def8bc5074a18931a050fec069bee59dfc8
Stuart Mackenzie <ilovediscostu@gmail.com>
2023-10-20 21:26:56 +0100

    update

--- src/components/podcast.js
+++ src/components/podcast.js
@@ -8,32 +8,34 @@ const Podcast = ({ audio }) => {
           <source src={audio} type="audio/mpeg" />
           Your browser does not support the audio tag.
         </audio>
-      </div>
-      <div className="tags is-pulled-right mx-5">
-        <a
-          className="tag is-small is-success"
-          href="https://podcasts.apple.com/us/podcast/hello-stu/id1709876510"
-        >
-          Apple
-        </a>
-        <a
-          className="tag is-small is-success"
-          href="https://open.spotify.com/show/21LDbQx6BlTudyfoT3oQH5"
-        >
-          Spotify
-        </a>
-        <a
-          className="tag is-small is-success"
-          href="https://podcasts.google.com/feed/aHR0cHM6Ly9oZWxsb3N0dS54eXovcG9kY2FzdC54bWw"
-        >
-          Google
-        </a>
-        <a
-          className="tag is-small is-success"
-          href="https://hellostu.xyz/podcast.xml"
-        >
-          Feed
-        </a>
+        <div className="tags">
+          <div className="tag is-light is-rounded">
+            <span className="mr-3">
+              Subscribe to <strong>HelloStu</strong> podcast:
+            </span>
+            <a
+              className="mr-3"
+              href="https://podcasts.apple.com/us/podcast/hello-stu/id1709876510"
+            >
+              Apple Podcasts
+            </a>
+            <a
+              className="mr-3"
+              href="https://open.spotify.com/show/21LDbQx6BlTudyfoT3oQH5"
+            >
+              Spotify
+            </a>
+            <a
+              className="mr-3"
+              href="https://podcasts.google.com/feed/aHR0cHM6Ly9oZWxsb3N0dS54eXovcG9kY2FzdC54bWw"
+            >
+              Google Podcasts
+            </a>
+            <a className="" href="https://hellostu.xyz/podcast.xml">
+              Podcast Feed
+            </a>
+          </div>
+        </div>
       </div>
     </>
   )
