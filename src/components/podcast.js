9977978d9adcfe473d2e2e7bce39ad77f9b254cf
d1sc0 <ilovediscostu@gmail.com>
2023-10-20 17:03:02 +0100

    update pod

--- src/components/podcast.js
+++ src/components/podcast.js
@@ -2,24 +2,40 @@ import * as React from 'react'
 
 const Podcast = ({ audio }) => {
   return (
-    <div className="imageFull">
-      <audio controls className="audioplayer">
-        <source src={audio} type="audio/mpeg" />
-        Your browser does not support the audio tag.
-      </audio>
-      <small>
-        Subscribe in your podcast player:{' '}
-        <a href="https://podcasts.apple.com/us/podcast/hello-stu/id1709876510">
+    <>
+      <div className="imageFull">
+        <audio controls className="audioplayer">
+          <source src={audio} type="audio/mpeg" />
+          Your browser does not support the audio tag.
+        </audio>
+      </div>
+      <div className="tags is-pulled-right mx-5">
+        <a
+          className="tag is-small is-success"
+          href="https://podcasts.apple.com/us/podcast/hello-stu/id1709876510"
+        >
           Apple
-        </a>{' '}
-        |{' '}
-        <a href="https://open.spotify.com/show/21LDbQx6BlTudyfoT3oQH5">
+        </a>
+        <a
+          className="tag is-small is-success"
+          href="https://open.spotify.com/show/21LDbQx6BlTudyfoT3oQH5"
+        >
           Spotify
-        </a>{' '}
-        | <a href="#">Google (TBC)</a> |{' '}
-        <a href="https://hellostu.xyz/podcast.xml">Other (RSS)</a>
-      </small>
-    </div>
+        </a>
+        <a
+          className="tag is-small is-success"
+          href="https://podcasts.google.com/feed/aHR0cHM6Ly9oZWxsb3N0dS54eXovcG9kY2FzdC54bWw"
+        >
+          Google
+        </a>
+        <a
+          className="tag is-small is-success"
+          href="https://hellostu.xyz/podcast.xml"
+        >
+          Feed
+        </a>
+      </div>
+    </>
   )
 }
 
