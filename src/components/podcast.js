35c73e6ff3f890cf8a9d6f6cd327b5394237195f
Stuart Mackenzie <ilovediscostu@gmail.com>
2023-10-20 21:35:26 +0100

    update

--- src/components/podcast.js
+++ src/components/podcast.js
@@ -9,32 +9,33 @@ const Podcast = ({ audio }) => {
           Your browser does not support the audio tag.
         </audio>
         <div className="tags">
-          <div className="tag is-light is-rounded">
-            <span className="mr-3">
-              Subscribe to <strong>HelloStu</strong> podcast:
-            </span>
-            <a
-              className="mr-3"
-              href="https://podcasts.apple.com/us/podcast/hello-stu/id1709876510"
-            >
-              Apple Podcasts
-            </a>
-            <a
-              className="mr-3"
-              href="https://open.spotify.com/show/21LDbQx6BlTudyfoT3oQH5"
-            >
-              Spotify
-            </a>
-            <a
-              className="mr-3"
-              href="https://podcasts.google.com/feed/aHR0cHM6Ly9oZWxsb3N0dS54eXovcG9kY2FzdC54bWw"
-            >
-              Google Podcasts
-            </a>
-            <a className="" href="https://hellostu.xyz/podcast.xml">
-              Podcast Feed
-            </a>
-          </div>
+          <span className="tag is-light is-rounded">
+            Subscribe to <strong>&nbsp;HelloStu&nbsp;</strong> podcast:
+          </span>
+          <a
+            className="tag is-light is-rounded"
+            href="https://podcasts.apple.com/us/podcast/hello-stu/id1709876510"
+          >
+            Apple Podcasts
+          </a>
+          <a
+            className="tag is-light is-rounded"
+            href="https://open.spotify.com/show/21LDbQx6BlTudyfoT3oQH5"
+          >
+            Spotify
+          </a>
+          <a
+            className="tag is-light is-rounded"
+            href="https://podcasts.google.com/feed/aHR0cHM6Ly9oZWxsb3N0dS54eXovcG9kY2FzdC54bWw"
+          >
+            Google Podcasts
+          </a>
+          <a
+            className="tag is-light is-rounded"
+            href="https://hellostu.xyz/podcast.xml"
+          >
+            Podcast Feed
+          </a>
         </div>
       </div>
     </>
