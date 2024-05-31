1c6f646ff32c7c9f6c600be76c5d83f59a9965cc
Stuart Mackenzie <ilovediscostu@gmail.com>
2024-05-31 23:03:01 +0100

    making rss full posts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection('posts', ({ data }) => {
@@ -12,22 +15,15 @@ export async function GET(context) {
     description:
       'Hello Stu is the personal website and blog of Stuart Mackenzie. Stuart works hard to improve and transform public services as a Senior Partner at TPXimpact. Outside of work he is a photographer, amatuer web developer, runner, parkrun enthsiast and excotic disco dancer!',
     site: context.site,
-    xmlns: {
-      media: 'http://search.yahoo.com/mrss/',
-    },
     items: posts.map(post => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/posts/${post.slug}/`,
-      customData: `<media:content
-          type="image/${
-            post.data.postImage.src.format == 'jpg' ? 'jpeg' : 'png'
-          }"
-          width="800"
-          medium="image"
-          url="https://hellostu.xyz${post.data.postImage.src.src}" />
-      `,
+      // Note: this will not process components or JSX expressions in MDX files.
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
     })),
   });
 }
