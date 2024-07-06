cfda01c9f1137010188e8706a758dd7bccd93569
Stuart Mackenzie <ilovediscostu@gmail.com>
2024-07-07 00:18:42 +0100

    update rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,18 +12,24 @@ export async function GET(context) {
     description:
       'Hello Stu is the personal website and blog of Stuart Mackenzie. Stuart works hard to improve and transform public services as a Senior Partner at TPXimpact. Outside of work he is a photographer, amatuer web developer, runner, parkrun enthsiast and excotic disco dancer!',
     site: context.site,
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+      atom: 'http://www.w3.org/2005/Atom',
+    },
+    customData: `<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />`,
     items: posts.map(post => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: `${post.data.description} <a href="/posts/${post.slug}">[...]</a>`,
       author: 'Stuart Mackenzie',
       link: `/posts/${post.slug}/`,
-      enclosure: {
-        url: post.data.postImage.src,
-        type: 'image/' + post.data.postImage.format,
-        width: post.data.postImage.width,
-        height: post.data.postImage.height,
-      },
+      customData: `<media:content
+          type="image/${post.data.postImage.format == 'jpg' ? 'jpeg' : 'png'}"
+          width="${post.data.postImage.width}"
+          height="${post.data.postImage.height}"
+          medium="image"
+          url="${context.site + post.data.postImage.src}" />
+      `,
     })),
   });
 }
